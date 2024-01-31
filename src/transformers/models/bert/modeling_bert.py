e8d8fff60fb9bdae4ff52839d65723674b113901
GhadeerJaradat2 <52407095+GhadeerJaradat2@users.noreply.github.com>
2024-01-31 13:28:41 +0400

    Update modeling_bert.py

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -420,7 +420,7 @@ class BertSelfAttention(nn.Module):
         #print("THETA 6 MSB")
         global TotalNumOfHeads
         global RemovedHeads
-        thresholdVal=5000
+        thresholdVal=6000
         thetaL0=thresholdVal
         thetaL1=thresholdVal
         thetaL2=thresholdVal
