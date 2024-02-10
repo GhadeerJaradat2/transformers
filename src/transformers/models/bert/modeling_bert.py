de50fb093ed56900e13534234fad16b5d4ffc535
GhadeerJaradat2 <52407095+GhadeerJaradat2@users.noreply.github.com>
2024-02-10 21:09:52 +0400

    Update modeling_bert.py

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -485,9 +485,9 @@ class BertSelfAttention(nn.Module):
         mean_sum_tensor= torch.mean(sum_tensor,3)
         #print("mean_sum_tensor",mean_sum_tensor)
         if(PruningRatio.Layerno%12>=0):
-            if( PruningRatio.PruningRatio>=0 and PruningRatio.PruningRatio<1):
+            if( PruningRatio.PruningRatio>=0 and PruningRatio.PruningRatio<=1):
                 threshold=torch.add(torch.mul(max_sum_tensor,PruningRatio.PruningRatio) , (torch.mul(mean_sum_tensor,(1-PruningRatio.PruningRatio)) ))
-            elif ( PruningRatio.PruningRatio>-1 and PruningRatio.PruningRatio<0 ):
+            elif ( PruningRatio.PruningRatio>=-1 and PruningRatio.PruningRatio<0 ):
                 threshold=torch.add(torch.mul(min_sum_tensor,-PruningRatio.PruningRatio) ,(torch.mul(mean_sum_tensor,(1+PruningRatio.PruningRatio))))
         else:
             threshold=min_sum_tensor
