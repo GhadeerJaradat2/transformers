a62ab695afc386f3612377eac4400c65e97f6f1c
GhadeerJaradat2 <52407095+GhadeerJaradat2@users.noreply.github.com>
2025-03-21 14:59:22 +0400

    Update modeling_bert.py

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """PyTorch BERT model."""
-ptint("TEST")
+print("TEST")
 from transformers.models.bert import HyperParameters
 import numpy as np
 import time
