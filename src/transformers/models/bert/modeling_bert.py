d4bc6624cf1d100846f18889f4eee9c3fa50b085
GhadeerJaradat2 <52407095+GhadeerJaradat2@users.noreply.github.com>
2025-03-24 11:42:56 +0400

    Update modeling_bert.py

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+print("softmax iterate input, changable step size")
 """PyTorch BERT model."""
 from transformers.models.bert import HyperParameters
 import numpy as np
